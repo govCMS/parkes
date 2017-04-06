fdbbd2d9bfbd6bf18bf1b69fcc0905b6dfa3a2a4
Alex Page <alex@alexpage.com.au>
2017-04-06 11:08:08 +1000

    Restructuring SASS, removing unecessary gulp tasks, removing linting, removing font awesome.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,11 @@
-'use strict';
+var gulp = require('gulp');
+var del = require('del');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var bs = require('browser-sync');
 
-var importOnce = require('node-sass-import-once'),
-  path = require('path');
-
+var $ = gulpLoadPlugins();
 var options = {};
 
-// #############################
-// Edit these paths and options.
-// #############################
-
-// The root paths are used to construct all the other paths in this
-// configuration. The "project" root path is where this gulpfile.js is located.
-// While Zen distributes this in the theme root folder, you can also put this
-// (and the package.json) in your project's root folder and edit the paths
-// accordingly.
 options.rootPath = {
   project: __dirname + '/',
   theme: __dirname + '/'
@@ -22,207 +14,73 @@ options.rootPath = {
 options.theme = {
   root: options.rootPath.theme,
   css: options.rootPath.theme + 'css/',
-  sass: options.rootPath.theme + 'sass/',
-  js: options.rootPath.theme + 'js/'
+  sass: options.rootPath.theme + 'sass/'
 };
+
+var sassFiles = [
+  options.theme.sass + '**/*.scss',
+  // Do not open Sass partials as they will be included as needed.
+  '!' + options.theme.sass + '**/_*.scss',
+  // Hide additional files
+  '!' + options.theme.sass + 'init/uikit/uikit.scss'
+];
 
 // Set the URL used to access the Drupal website under development. This will
 // allow Browser Sync to serve the website and update CSS changes on the fly.
-options.drupalURL = 'http://govcms-parkes.local/';
-// options.drupalURL = 'http://localhost';
+options.drupalURL = '';
+// options.drupalURL = 'localhost'
 
-// Define the node-sass configuration. The includePaths is critical!
-options.sass = {
-  importer: importOnce,
-  includePaths: [
-    options.theme.sass + 'styles.scss'
-  ],
-  outputStyle: 'expanded'
-};
-
-// Define which browsers to add vendor prefixes for.
-options.autoprefixer = {
-  browsers: [
-    "last 2 versions",
-    "ie 8",
-    "ie 9",
-    "ie 10"
-  ]
-};
-
-// Define the paths to the JS files to lint.
-options.eslint = {
-  files: [
-    options.theme.js + '**/*.js',
-    '!' + options.theme.js + '**/*.min.js'
-  ]
-};
-
-// Define the paths to the SASS files to lint.
-options.sasslint = {
-  files: [
-    options.theme.sass + '**/*.scss',
-    '!' + options.theme.sass + 'pancake/*.scss',
-    '!' + options.theme.sass + 'vendor/**/*.scss'
-  ]
-};
-
-// If your files are on a network share, you may want to turn on polling for
-// Gulp watch. Since polling is less efficient, we disable polling by default.
-options.gulpWatchOptions = {};
-// options.gulpWatchOptions = {interval: 1000, mode: 'poll'};
-
-
-// ################################
-// Load Gulp and tools we will use.
-// ################################
-var gulp = require('gulp'),
-  $ = require('gulp-load-plugins')(),
-  browserSync = require('browser-sync').create(),
-  del = require('del'),
-  // gulp-load-plugins will report "undefined" error unless you load gulp-sass manually.
-  sass = require('gulp-sass');
-
-// The default task.
-gulp.task('default', ['build']);
-
-// #################
-// Build everything.
-// #################
-gulp.task('build', ['styles:production', 'lint']);
-
-// ##########
-// Build CSS.
-// ##########
-var sassFiles = [
-  options.theme.sass + 'styles.scss',
-  // Do not open Sass partials as they will be included as needed.
-  '!' + options.theme.sass + '**/_*.scss'
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
 ];
 
-gulp.task('styles', ['clean:css'], function () {
-  return gulp.src(sassFiles)
-    .pipe($.sourcemaps.init())
-    .pipe(sass(options.sass).on('error', sass.logError))
-    .pipe($.autoprefixer(options.autoprefixer))
-    .pipe($.size({
-      showFiles: true
-    }))
-    .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest(options.theme.css))
-    .pipe($.if(browserSync.active, browserSync.stream({
-      match: '**/*.css'
-    })));
+// Clean all directories.
+gulp.task('clean', ['clean:css']);
+
+// Clean CSS files.
+gulp.task('clean:css', function() {
+  return del([
+      options.theme.css + '**/*.css',
+      options.theme.css + '**/*.map'
+    ], {force: true});
 });
 
-gulp.task('styles:production', ['clean:css'], function () {
+// Compile and automatically prefix stylesheets
+gulp.task('styles', ['clean:css'], function(){
+
   return gulp.src(sassFiles)
-    .pipe(sass(options.sass).on('error', sass.logError))
-    .pipe($.autoprefixer(options.autoprefixer))
-    .pipe($.size({
-      showFiles: true
-    }))
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      precision: 10,
+      outputStyle: 'compressed'
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe($.sourcemaps.write('./'))
+    .pipe($.size({title: 'Styles'}))
     .pipe(gulp.dest(options.theme.css));
 });
 
-// #########################
-// Lint Sass and JavaScript.
-// #########################
-gulp.task('lint', ['lint:sass', 'lint:js']);
 
-// Lint JavaScript.
-gulp.task('lint:js', function () {
-  return gulp.src(options.eslint.files)
-    .pipe($.eslint({
-      rules: {
-        'no-shadow-restricted-names': 0,
-        'no-undefined': 0
-      }
-    }))
-    .pipe($.eslint.format());
-});
-
-// Lint JavaScript and throw an error for a CI to catch.
-gulp.task('lint:js-with-fail', function () {
-  return gulp.src(options.eslint.files)
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-    .pipe($.eslint.failOnError());
-});
-
-// Lint Sass.
-gulp.task('lint:sass', function () {
-  return gulp.src(options.sasslint.files)
-    .pipe($.sassLint())
-    .pipe($.sassLint.format());
-});
-
-// Lint Sass and throw an error for a CI to catch.
-gulp.task('lint:sass-with-fail', function () {
-  return gulp.src([options.theme.sass])
-    .pipe($.sassGlob())
-    .pipe($.sassLint())
-    .pipe($.sassLint.format())
-    .pipe($.sassLint.failOnError());
-});
-
-// ##############################
-// Watch for changes and rebuild.
-// ##############################
-gulp.task('watch', ['browser-sync', 'watch:lint', 'watch:js']);
-
-gulp.task('browser-sync', ['watch:css'], function () {
-  if (!options.drupalURL) {
-    return Promise.resolve();
-  }
-  return browserSync.init({
+gulp.task('browser-sync', function() {
+  bs.init({
     proxy: options.drupalURL,
-    noOpen: false
+    open: false
   });
 });
 
-gulp.task('watch:css', ['styles'], function () {
-  return gulp.watch(options.theme.sass + '**/*.scss', options.gulpWatchOptions, ['styles']);
+// Watch files for changes & reload
+gulp.task('watch', ['browser-sync'], function(){
+  gulp.watch([options.theme.sass + '/**/*.scss'], ['styles', bs.reload]);
+  gulp.watch(['./templates/*.php', './*.php']).on('change', bs.reload);
 });
 
-gulp.task('watch:lint', ['lint:sass'], function () {
-  return gulp.watch([
-    options.theme.sass + '**/*.scss'
-  ], options.gulpWatchOptions, ['lint:sass']);
-});
-
-gulp.task('watch:js', ['lint:js'], function () {
-  return gulp.watch(options.eslint.files, options.gulpWatchOptions, ['lint:js']);
-});
-
-// ######################
-// Clean all directories.
-// ######################
-gulp.task('clean', ['clean:css', 'clean:styleguide']);
-
-// Clean style guide files.
-gulp.task('clean:styleguide', function () {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  return del([
-    options.styleGuide.destination + '*.html',
-    options.styleGuide.destination + 'public'
-  ], {
-    force: true
-  });
-});
-
-// Clean CSS files.
-gulp.task('clean:css', function () {
-  return del([
-    options.theme.css + '**/styles.css',
-    options.theme.css + '**/*.map'
-  ], {
-    force: true
-  });
-});
-
-
-// Resources used to create this gulpfile.js:
-// - https://github.com/google/web-starter-kit/blob/master/gulpfile.babel.js
-// - https://github.com/dlmanning/gulp-sass/blob/master/README.md
-// - http://www.browsersync.io/docs/gulp/
+// Watch and reload
+gulp.task('default', ['styles', 'watch']);
